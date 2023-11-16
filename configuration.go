828c1f738e3703aa43b9296428890d14e8ffe8db
Edson Michaque <edson@michaque.com>
2023-11-16 08:07:25 +0200

    Add comments

--- configuration.go
+++ configuration.go
@@ -4,12 +4,15 @@ import (
 	"context"
 )
 
+// Configuration
 type Configuration interface {
 	Configure(context.Context, *ConfigureOptions) error
 }
 
+// ConfigurationFunc
 type ConfigurationFunc func(context.Context) error
 
+// Configure
 func (c ConfigurationFunc) Configure(ctx context.Context) error {
 	return c(ctx)
 }
